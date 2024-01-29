f983af4296ee4a26d6f3b4ade4d94054efb99ecd
Kirstyn Canull <kirstyncanull@Kirstyns-MacBook-Air.local>
2024-01-29 11:12:56 -0500

    finally submitting

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,5 @@ import React from "react";
 function Header(props) {
       return <h1>hello from the Header!</h1>;
 }
-function makeAP(p) {
-  return <p>please pass this test</p>
-}
-makeAP();
+
 export default Header;
